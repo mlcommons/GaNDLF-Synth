74f1ab40d68d607c854903555fe3975f6f3d9d06
szmazurek <48680634+szmazurek@users.noreply.github.com>
2024-05-10 13:54:43 +0200

    Implementing basic extractors
    - implemented extractors for unlabeled and single-label data
    - the approach for now assumes only single channel is passed
    - need to think if we want to handle multichannel training (I tought
      about the channels more like labels in conditional case)
    - minor update for base abstraction to also incorporate the channel_id
      in the constructor

--- gandlf_synth/data/csv_data_extractor.py
+++ gandlf_synth/data/csv_data_extractor.py
@@ -1,8 +1,39 @@
 import os
-from abc import ABC, abstractmethod
-
-import pandas as pd
+from glob import glob
 from pathlib import Path
+from abc import ABC, abstractmethod
+from typing import List
+import pandas as pd
+
+
+def append_row_to_dataframe(dataframe: pd.DataFrame, row: list):
+    """
+    Append a row to the dataframe inplace.
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to which the row will be appended.
+        row (list): The row to be appended.
+    """
+    dataframe.loc[len(dataframe)] = row
+
+
+def find_files_in_dir(directory: str, channel_id: str) -> List[str]:
+    """
+    Find all files in a directory that contain the channel ID. Sorts
+    them based on the parent directory name.
+
+    Args:
+        directory (str): The path to the directory.
+        channel_id (str): The channel ID.
+
+    Returns:
+        List[str]: A list of files that contain the channel ID.
+    """
+
+    channel_files = glob(f"{directory}/**/*{channel_id}", recursive=True)
+    assert channel_files, f"No files found for channel {channel_id}"
+    channel_files.sort(key=lambda x: os.path.basename(os.path.dirname(x)))
+    return channel_files
 
 
 class CSVDataExtractor(ABC):
@@ -24,6 +55,7 @@ class CSVDataExtractor(ABC):
         super().__init__()
         self.dataset_path = Path(dataset_path)
         self.channel_id = channel_id
+        self.channel_id_list = channel_id.split(",")
 
     @abstractmethod
     def _extract_data(self, dataset_path: Path) -> pd.DataFrame:
@@ -49,3 +81,106 @@ class CSVDataExtractor(ABC):
         """
         data = self._extract_data(self.dataset_path)
         self._save_csv(data, output_path)
+
+
+class UnlabeledDataExtractor(CSVDataExtractor):
+    """
+    Extractor for the case when the user operates in unconditional mode.
+    Reads all image files from a given directory and stores them in a CSV file
+    in GaNDLF standard format.
+
+    """
+
+    def _extract_data(self, dataset_path: Path) -> pd.DataFrame:
+        """
+        Extract data from the CSV file.
+
+        Args:
+            dataset_path (Path): The path to the dataset.
+
+        Returns:
+            pd.DataFrame: The extracted data.
+        """
+        dataframe = pd.DataFrame(
+            columns=[f"Channel_{i}" for i in range(len(self.channel_id_list))]
+        )
+
+        # find all image files in the dataset based on the channel IDs
+        # TODO this works for multiple channels, but assumes the same
+        # number of files for each channel. Need to think if we really
+        # need to support multiple channels.
+        # files_for_channels = {
+        #     channel_id: find_files_in_dir(dataset_path, channel_id)
+        #     for channel_id in self.channel_id_list
+        # }
+        # rows = list((zip(*files_for_channels.values())))
+        # rows = [list(row) for row in rows]
+        # [append_row_to_dataframe(dataframe, row) for row in rows]
+
+        # for now I assume we accept only one channel
+        files = find_files_in_dir(dataset_path, self.channel_id)
+        [append_row_to_dataframe(dataframe, [file]) for file in files]
+        return dataframe
+
+
+class LabeledDataExtractor(CSVDataExtractor):
+    """
+    Extractor for the case when the user operates in conditional mode.
+    Reads all image files and creates labels for them. The labels are
+    extracted from the names of directories containing the image files.
+    Example directory structure:
+    dataset
+    ├── class1
+    │   ├── image1.nii.gz
+    │   ├── image2.nii.gz
+        ├── class2
+    │   ├── image3.nii.gz
+    │   ├── image4.nii.gz
+    etc.
+    We require user to place images in directories named after the class.
+    """
+
+    def _extract_data(self, dataset_path: Path) -> pd.DataFrame:
+        """
+        Extract data from the CSV file.
+
+        Args:
+            dataset_path (Path): The path to the dataset.
+
+        Returns:
+            pd.DataFrame: The extracted data.
+        """
+        dataframe = pd.DataFrame(
+            columns=[f"Channel_{i}" for i in range(len(self.channel_id_list))]
+            + ["Label"]
+            + ["LabelMapping"]
+        )
+        files = find_files_in_dir(dataset_path, self.channel_id)
+        unique_dirnames = list(set([Path(file).parent.name for file in files]))
+        dirnames_to_labels = {dirname: i for i, dirname in enumerate(unique_dirnames)}
+        [
+            append_row_to_dataframe(
+                dataframe,
+                [
+                    file,
+                    dirnames_to_labels[Path(file).parent.name],
+                    Path(file).parent.name,
+                ],
+            )
+            for file in files
+        ]
+        return dataframe
+
+
+if __name__ == "__main__":
+    dataset_path = "testing/data/unlabeled"
+    channel_id = ".nii.gz"
+    output_path = "output.csv"
+    extractor = UnlabeledDataExtractor(dataset_path, channel_id)
+    extractor.extract_csv_data(output_path)
+
+    dataset_path = "testing/data/labeled"
+    channel_id = ".nii.gz"
+    output_path = "output_labeled.csv"
+    extractor = LabeledDataExtractor(dataset_path, channel_id)
+    extractor.extract_csv_data(output_path)
