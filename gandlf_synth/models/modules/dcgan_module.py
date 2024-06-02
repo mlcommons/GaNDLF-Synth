7234e254970aaa4bcb96fbc966a7315710819950
szmazurek <48680634+szmazurek@users.noreply.github.com>
2024-06-02 16:29:05 +0200

    Implementing training step for DCGAN module

--- gandlf_synth/models/modules/dcgan_module.py
+++ gandlf_synth/models/modules/dcgan_module.py
@@ -1,3 +1,5 @@
+import warnings
+
 import torch
 from torch import nn
 from torch.optim.lr_scheduler import _LRScheduler
@@ -16,12 +18,76 @@ from typing import Dict, Union
 class UnlabeledDCGANModule(SynthesisModule):
     def training_step(self, batch: object, batch_idx: int) -> torch.Tensor:
         real_images = self._ensure_device_placement(batch)
-        batch_size = real_images.size(0)
+        batch_size = real_images.shape[0]
+        latent_vector = generate_latent_vector(
+            batch_size,
+            self.model_config.latent_vector_size,
+            self.model_config.n_dimensions,
+            self.device,
+        )
+        # DISCRIMINATOR PASS WITH REAL IMAGES
+        self.optimizers["disc_optimizer"].zero_grad(set_to_none=True)
         label_real = torch.full(
             (batch_size,), fill_value=1.0, dtype=torch.float, device=self.device
         )
         preds_real = self.model.discriminator(real_images)
         disc_loss_real = self.losses["disc_loss"](preds_real, label_real)
+        backward_pass(
+            loss=disc_loss_real,
+            optimizer=self.optimizers["disc_optimizer"],
+            model=self.model.discriminator,
+            amp=self.model_config.amp,
+            clip_grad=self.model_config.clip_grad,
+            clip_mode=self.model_config.clip_mode,
+        )
+
+        # DISCRIMINATOR PASS WITH FAKE IMAGES
+        label_fake = label_real.fill_(0.0)  # swap the labels for fake images
+        fake_images = self.model.generator(latent_vector)
+        preds_fake = self.model.discriminator(fake_images.detach())
+        disc_loss_fake = self.losses["disc_loss"](preds_fake, label_fake)
+        backward_pass(
+            loss=disc_loss_fake,
+            optimizer=self.optimizers["disc_optimizer"],
+            model=self.model.discriminator,
+            amp=self.model_config.amp,
+            clip_grad=self.model_config.clip_grad,
+            clip_mode=self.model_config.clip_mode,
+        )
+        is_any_nan = torch.isnan(disc_loss_real) or torch.isnan(disc_loss_fake)
+        if not is_any_nan:
+            # is this correct? is it propagating correctly?
+            loss_disc = disc_loss_real + disc_loss_fake
+            self.optimizers["disc_optimizer"].step()
+            self.optimizers["disc_optimizer"].zero_grad(set_to_none=True)
+        else:
+            warnings.warn(
+                f"NaN loss detected in discriminator step for batch {batch_idx}, the step will be skipped",
+                RuntimeWarning,
+            )
+
+        # GENERATOR PASS
+        self.optimizers["gen_optimizer"].zero_grad(set_to_none=True)
+        label_real.fill_(1.0)  # swap the labels for fake images
+        preds_fake = self.model.discriminator(fake_images)
+        gen_loss = self.losses["gen_loss"](preds_fake, label_real)
+        backward_pass(
+            loss=gen_loss,
+            optimizer=self.optimizers["gen_optimizer"],
+            model=self.model.generator,
+            amp=self.model_config.amp,
+            clip_grad=self.model_config.clip_grad,
+            clip_mode=self.model_config.clip_mode,
+        )
+        is_any_nan = torch.isnan(gen_loss)
+        if not is_any_nan:
+            self.optimizers["gen_optimizer"].step()
+            self.optimizers["gen_optimizer"].zero_grad(set_to_none=True)
+        else:
+            warnings.warn(
+                f"NaN loss detected in generator step for batch {batch_idx}, the step will be skipped",
+                RuntimeWarning,
+            )
 
     def _initialize_model(self) -> ModelBase:
         return DCGAN(self.model_config)
