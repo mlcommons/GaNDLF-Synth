05ff9b02ff4d44cf209c3674212a7e85c4f9b99b
szmazurek <48680634+szmazurek@users.noreply.github.com>
2024-06-28 12:02:53 +0200

    Extending setup and other related files

--- setup.py
+++ setup.py
@@ -1,4 +1,92 @@
+import re
+import os
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
 
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+
+
+class CustomEggInfoCommand(egg_info):
+    def run(self):
+        egg_info.run(self)
+
+
+try:
+    with open("README.md") as readme_file:
+        readme = readme_file.read()
+except Exception as error:
+    readme = "No README information found."
+    sys.stderr.write("Warning: Could not open '%s' due %s\n" % ("README.md", error))
+
+
+try:
+    filepath = "GANDLF/version.py"
+    version_file = open(filepath)
+    (__version__,) = re.findall('__version__ = "(.*)"', version_file.read())
+
+except Exception as error:
+    __version__ = "0.0.1"
+    sys.stderr.write("Warning: Could not open '%s' due %s\n" % (filepath, error))
+
+dockerfiles = [
+    item
+    for item in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+    if (os.path.isfile(item) and item.startswith("Dockerfile-"))
+]
+entrypoint_files = [
+    item
+    for item in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+    if (os.path.isfile(item) and item.startswith("gandlf-sytnh_"))
+]
+setup_files = ["setup.py", ".dockerignore", "pyproject.toml", "MANIFEST.in"]
+
+all_extra_files = dockerfiles + entrypoint_files + setup_files
+all_extra_files_pathcorrected = [os.path.join("../", item) for item in all_extra_files]
+requirements = ["GANDLF@git+https://github.com/mlcommons/GandLF.git@master"]
 if __name__ == "__main__":
-    setup(name="gandlf_synth", version="0.1", packages=find_packages())
+    setup(
+        name="gandlf_synth",
+        version=__version__,
+        author="MLCommons",
+        author_email="mail@mlcommons.org",
+        python_requires=">=3.8, <3.12",
+        packages=find_packages(where=os.path.dirname(os.path.abspath(__file__))),
+        cmdclass={
+            "install": CustomInstallCommand,
+            "develop": CustomDevelopCommand,
+            "egg_info": CustomEggInfoCommand,
+        },
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: Apache Software License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        ],
+        description=(
+            "PyTorch-based framework that handles image synthesis using various DL architectures for medical imaging."
+        ),
+        install_requires=requirements,
+        license="Apache-2.0",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        include_package_data=True,
+        package_data={"gandlf-synth": all_extra_files_pathcorrected},
+        keywords="image generation, image synthesis, data-augmentation, medical-imaging, clinical-workflows, deep-learning, pytorch",
+        zip_safe=False,
+    )
